79f9fb12a0523b8a79fa14afffbee5a67e4ec04f
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-17 22:52:37 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
-"""Log Parsing"""
-
 import sys
 
-# Define a dictionary to keep track of status codes and their counts
-status_codes = (
-        {
-            "200": 0,
-            "301": 0,
-            "400": 0,
-            "401": 0,
-            "403": 0,
-            "404": 0,
-            "405": 0,
-            "500": 0
-            }
-        )
-total_size = 0  # Initialize total file size
-line_count = 0  # Initialize line count
-
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
+line_count = 0
 
 def print_metrics():
-    """
-    Print the metrics including total file size and status code counts.
-    """
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code])
+            print("{}: {}".format(code, status_codes[code]))
 
 try:
     for line in sys.stdin:

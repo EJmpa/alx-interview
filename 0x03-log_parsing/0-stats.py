38f08efafeed8bbc087e35149698d89ca7cf9fff
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-17 22:00:56 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 import sys
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = (
+        {
+            "200": 0, "301": 0, "400": 0,
+            "401": 0, "403": 0, "404": 0, "405": 0, "500": 0
+            }
+        )
 total_size = 0
 line_count = 0
+
 
 def print_metrics():
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
+
 
 try:
     for line in sys.stdin:

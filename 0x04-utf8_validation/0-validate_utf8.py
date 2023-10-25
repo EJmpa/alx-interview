fa9893a9646fce4b5aafd03e9ddc120c99ff5750
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-25 18:56:05 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""UTF-8 Encoding"""
+"""UFT-8 Encoding"""
 
 from typing import List
 
 
 def validUTF8(data: List[int]) -> bool:
     """
-    Determine if a given list of integers represents a valid UTF-8 encoding.
+    Check if a given list of integers represents a valid UTF-8 encoding.
 
     Args:
         data (List[int]): A list of integers representing bytes.
@@ -14,33 +14,26 @@ def validUTF8(data: List[int]) -> bool:
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    # Initialize a variable to count the number of
-    # bytes for the current character
-    num_bytes = 0
+    count = 0  # Initialize a variable to keep track of the number of bytes
+    # remaining for the current character
 
-    for byte in data:
-        # Check if the 8th bit is set (i.e., if it's not in the range [0, 127])
-        if byte >= 128:
-            # Check if it's a continuation byte (binary: 10xxxxxx)
-            if num_bytes == 0:
-                return False
-
-            # Decrement the count of remaining bytes
-            num_bytes -= 1
-        else:
+    for num in data:
+        if count == 0:
             # Determine the number of bytes required for the current character
-            if num_bytes > 0:
+            if (num >> 5) == 0b110:
+                count = 1
+            elif (num >> 4) == 0b1110:
+                count = 2
+            elif (num >> 3) == 0b11110:
+                count = 3
+            elif (num >> 7):
+                # If the 8th bit is set, it's not a valid start of a character
                 return False
-            if byte < 128:
-                num_bytes = 0
-            elif byte < 192:  # Binary: 110xxxxx
-                num_bytes = 1
-            elif byte < 224:  # Binary: 1110xxxx
-                num_bytes = 2
-            elif byte < 240:  # Binary: 11110xxx
-                num_bytes = 3
-            else:
+        else:
+            # For continuation bytes, the 2nd highest bit should be '10'
+            if (num >> 6) != 0b10:
                 return False
+            count -= 1  # Decrement the count of remaining bytes for
+            # the current character
 
-    # Check if all characters are properly terminated
-    return num_bytes == 0
+    return count == 0  # All characters are valid if count is back to 0
